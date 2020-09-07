ad390151fe2f76ffb64910400dc2d6c2fcace27b
ashish60808 <ashish60808@gmail.com>
2020-09-07 19:54:25 +0530

    Adding the images to repo

--- js/projects.js
+++ js/projects.js
@@ -6,7 +6,7 @@ oss_projects['python-terraform'] = {
   featured: true,
   position: 1,
   background: 'emrio_bg.png'
-};
+}
 oss_projects['golang-rest_apis'] = {
   blog_post: 'https://github.com/ashish60808/golang-rest_apis/blob/master/README.md',
   featured: true,
@@ -14,18 +14,18 @@ oss_projects['golang-rest_apis'] = {
   background: 'paasta_bg.png'
 };
 oss_projects['ShellScripts'] = {
-  blog_post: 'https://github.com/ashish60808/ShellScripts/blob/master/README.md'
+  blog_post: 'https://github.com/ashish60808/ShellScripts/blob/master/README.md',
   featured: true,
   position: 4,
   background: 'dumb-init.png'
-};
+}
 
 oss_projects['Code'] = {
-  blog_post: 'https://github.com/ashish60808/Code/blob/master/README.md'
+  blog_post: 'https://github.com/ashish60808/Code/blob/master/README.md',
   featured: true,
   position: 3,
   background: 'mrjob_bg-.png'
-};
+}
 
 
 oss_projects['ansible'] = { deprecated: true }
