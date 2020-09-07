1099e3ef3bf57a0fa8f5dc744b08c072483e45e5
ashish60808 <ashish60808@gmail.com>
2020-09-07 20:14:24 +0530

    Adding test image

--- js/projects.js
+++ js/projects.js
@@ -11,7 +11,7 @@ oss_projects['golang-rest_apis'] = {
   blog_post: 'https://github.com/ashish60808/golang-rest_apis/blob/master/README.md',
   featured: true,
   position: 2,
-  background: 'paasta_bg.png'
+  background: 'test.png'
 };
 oss_projects['ShellScripts'] = {
   blog_post: 'https://github.com/ashish60808/ShellScripts/blob/master/README.md',
