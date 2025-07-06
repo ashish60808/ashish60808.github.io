b38233f3dc30dd402142d492914228a42bd3c041
Santosh <santbhatsre@gmail.com>
2025-07-06 19:04:25 +0530

    Adding tech radar tab

---
+++ js/projects.js
@@ -0,0 +1,38 @@
+
+oss_projects = {};
+
+oss_projects['python-terraform'] = {
+  blog_post: 'https://github.com/ashish60808/python-terraform/blob/master/README.md',
+  featured: true,
+  position: 1,
+  background: 'emrio_bg.png'
+};
+oss_projects['golang-rest_apis'] = {
+  blog_post: 'https://github.com/ashish60808/golang-rest_apis/blob/master/README.md',
+  featured: true,
+  position: 4,
+  background: 'paasta_bg.png'
+};
+oss_projects['ShellScripts'] = {
+  blog_post: 'https://github.com/ashish60808/ShellScripts/blob/master/README.md',
+  featured: true,
+  position: 2,
+  background: 'dumb-init.png'
+};
+
+oss_projects['Code'] = {
+  blog_post: 'https://github.com/ashish60808/Code/blob/master/README.md',
+  featured: true,
+  position: 3,
+  background: 'mrjob_bg.png'
+};
+
+oss_projects['sre-interview-prep-guide'] = {
+  blog_post: 'https://ashish60808.github.io/blogs/sre_interview_prep_guide',
+  featured: true,
+  position: 5,
+  background: 'moe_bg.png'
+}; 
+
+//oss_projects['ansible'] = { deprecated: true }
+
